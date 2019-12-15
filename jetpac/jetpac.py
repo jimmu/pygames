75e5ac0b593904f4d9f624e611644bd0e4c7ed46
jimmu <jimmy@LAPTOP-5KUKOGJR.localdomain>
2019-12-15 15:01:54 +0000

    Create Platform class2

--- jetpac/jetpac.py
+++ jetpac/jetpac.py
@@ -23,6 +23,16 @@ JETPACK_STRENGTH=0.52
 global ySpeed
 rocketSpeed=0
 
+class Platform:
+    def __init__(self, left, top, width, height, colour):
+        self.left=left
+        self.top=top
+        self.width=width
+        self.height=height
+        self.right=left+width
+        self.bottom=top+height
+        self.colour=colour
+
 def draw(): # Pygame Zero draw function
     global allThePlatforms
     global gameState
@@ -38,26 +48,25 @@ def draw(): # Pygame Zero draw function
     rocketStage2.draw()
 
     for platform in allThePlatforms:
-        screen.draw.filled_rect((Rect((platform["left"], platform["top"]), (platform["width"], platform["height"]))), platform["colour"])
+        screen.draw.filled_rect((Rect((platform.left, platform.top), (platform.width, platform.height))), platform.colour)
  
 def update(): # Pygame Zero update function
     global ySpeed
     global gameState
     global rocketSpeed
 
-    topBound=getTopBound()
-    bottomBound=getBottomBound()
-    leftBound=getLeftBound()
-    rightBound=getRightBound()
+    if keyboard.left:
+        leftBound=getLeftBound()
+        player.x -= 2
+        if player.left < leftBound: player.left=leftBound;
+        if player.left <0: player.right =WIDTH;
 
-    if keyboard.left: player.x -= 2
-    if keyboard.right: player.x += 2
-
-    if player.right > rightBound: player.right=rightBound;
-    if player.left < leftBound: player.left=leftBound;
-    if player.right >WIDTH: player.left=0
-    if player.left <0: player.right =WIDTH;
-
+    if keyboard.right:
+        rightBound=getRightBound()
+        player.x += 2
+        if player.right > rightBound: player.right=rightBound;
+        if player.right >WIDTH: player.left=0
+    
     if keyboard.up :
         ySpeed += JETPACK_STRENGTH
         player.image="playerwithjet"
@@ -66,17 +75,21 @@ def update(): # Pygame Zero update function
         
     #Now because of gravity
     ySpeed += GRAVITY
-    player.bottom -= ySpeed
 
-    #Don't let us fall through the land
-    if player.bottom >= bottomBound:
-        player.bottom = bottomBound
-        ySpeed=0
-
-    #Don't let us fly past the top of the screen
-    if player.top < topBound:
-        player.top = topBound
-        ySpeed=0
+    if ySpeed  <= 0:
+        bottomBound=getBottomBound()
+        #Don't let us fall through the land
+        player.bottom -= ySpeed
+        if player.bottom >= bottomBound:
+            player.bottom = bottomBound
+            ySpeed=0
+    else:
+        topBound=getTopBound()
+        player.bottom -= ySpeed
+        #Don't let us fly past the top of the screen
+        if player.top < topBound:
+            player.top = topBound
+            ySpeed=0
 
     #Have we picked up any rocket parts?
     if gameState == "lookingForStage1" :
@@ -127,68 +140,52 @@ def getTopBound():
     global allThePlatforms
     topBound=0
     for platform in allThePlatforms:
-        if ((player.top >= platform["top"]+platform["height"]) and
-            (player.right > platform["left"]) and
-            (player.left < (platform["left"]+platform["width"]))):
+        if ((player.top >= platform.bottom) and
+            (player.right > platform.left) and
+            (player.left < platform.right)):
             # We're directly below the platform
-            topBound=max(topBound, platform["top"]+platform["height"])
+            topBound=max(topBound, platform.bottom)
     return topBound
 
 def getBottomBound():
     global allThePlatforms
     bottomBound=HEIGHT
     for platform in allThePlatforms:
-        if ((player.bottom <= platform["top"]) and
-            (player.right > platform["left"]) and
-            (player.left < (platform["left"]+platform["width"]))):
+        if ((player.bottom <= platform.top) and
+            (player.right > platform.left) and
+            (player.left < (platform.right))):
             # We're directly above the platform
-            bottomBound=min(bottomBound, platform["top"])
+            bottomBound=min(bottomBound, platform.top)
     return bottomBound;
 
 def getLeftBound():
     global allThePlatforms
-    leftBound=0
+    leftBound = -1000
     for platform in allThePlatforms:
-        if ((player.left >= platform["left"]+platform["width"]) and
-            (player.bottom > platform["top"]) and
-            (player.top < (platform["top"]+platform["height"]))):
+        if ((player.left >= platform.right) and
+            (player.bottom > platform.top) and
+            (player.top < (platform.bottom))):
             # We're directly right of the platform
-            leftBound=max(leftBound, platform["left"]+platform["width"])
+            leftBound=max(leftBound, platform.right)
     return leftBound
 
 def getRightBound():
     global allThePlatforms
-    rightBound=WIDTH    
+    rightBound = WIDTH*2    
     for platform in allThePlatforms:
-        if ((player.right <= platform["left"]) and
-            (player.bottom > platform["top"]) and
-            (player.top < (platform["top"]+platform["height"]))):
+        if ((player.right <= platform.left) and
+            (player.bottom > platform.top) and
+            (player.top < (platform.bottom))):
             # We're directly left of the platform
-            rightBound=min(rightBound, platform["left"])
+            rightBound=min(rightBound, platform.left)
     return rightBound
 
 def makePlatforms():    
-    global platform, platform2, allThePlatforms
-    platform={"left" : 200,
-              "top" : 220,
-              "width" : 500,
-              "height" : 20,
-              "colour" : (0,175,0)
-              }
-    platform2={"left" : 300,
-              "top" : 520,
-              "width" : 300,
-              "height" : 20,
-              "colour" : (255, 255, 0)
-              }
-    platform3={"left" : 700,
-              "top" : 720,
-              "width" : 200,
-              "height" : 18,
-              "colour" : (2, 9, 244)
-              }
-    
-    allThePlatforms=[platform, platform2, platform3]
+    global allThePlatforms
+    allThePlatforms=[Platform(200, 220, 500, 20, (0, 175, 0)),
+                     Platform(300, 520, 300, 20, (255, 255, 0)),
+                     Platform(700, 720, 200, 18, (2, 9, 244))
+                    ]
                             
 # End of functions
 makePlatforms()
